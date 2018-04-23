24a00fcc4dce6a0554231e030c231b39f32ee0bf
Leandro Repolho <leandro.repolho@theiconic.com.au>
2018-04-23 15:19:02 +1000

    Change background colour

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@
         <title>PolarSeven</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #9CE305;}</style>
+        <style>body {margin-top: 40px; background-color: #1D9C21;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
@@ -22,7 +22,7 @@
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
-                        echo fread($myfile,filesize("/var/www/my-vol/date"));
+                        echo fread($myfile, filesize("/var/www/my-vol/date"));
                         fclose($myfile);
                 ?>
             </div>
